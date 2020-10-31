4e13742e539201a4a2c0f4e0767abb2abe9fb90c
Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>
2020-10-31 17:28:19 +0200

    старт
    Signed-off-by: Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,71 +1,168 @@
-// let hello = 'hello';
-// let owu = 'owu';
-// let _com = 'com';
-// alert(hello);
-// document.write(owu);
-// console.log(_com);
-// let a = '10';
-// let b = '-999';
-// let c = '3.14'
+
+
+// let nam = 'hello'
+// let one = 'owu'
+// let two = 'com'
+// let string = 'ua'
+// let num = 2929;
+// let number = -2.01;
 // let bool = true;
-// let boolean = false;
-// alert(a);
-// alert(boolean);
-// console.log(b);
-// document.write(c); // 1 && 2
-// let a = 292929;
-// let b = 202002;
-// let c = '1111';
-// let myName = 'Rus'
-// console.log(c);
-// console.log(b)
-// document.write(c);
-// // document.write(myName);  3 
-//  let name = prompt('What is your name?');
-//  let sur = prompt('What is your Sur name?');
-//  let full = prompt('What is your full name?');
-// document.write(name);
-// document.write(sur);
-// document.write(full);
-// let person = name, sur, full;
-//  let name = prompt('Ruslan');
-//  let sur = prompt('Marushka');
-//  let full = prompt('rusMaru');
-//  let name = 'Ruslan'
-//  document.write(name)
-// console.log(name)
-// let number = prompt(111)
-// // console.log(number)
-// let a = prompt('what this number?');
-// let number = '1000';
-// // console.log(number)
-// let name = prompt('What name?')
-// let surName = prompt('surName?')
-// let name = prompt('age')
-// let a = prompt('My name')
-// let b = prompt('Full name?');
-// let c = prompt('Sub name');
-// // document.write(a + name + b + fullName + c + subName)
+// bool = false;
 
-// // document.write('<h1>' + a + ' ' + b + ' '  + c + '</h1>')
-// let person = a + b + c;
+// console.log(bool)   1
+// let name = 'Ruslan'
+// let firstName = 'Marushka'
+// let age = 20;
+// console.log(name + ' ' +  firstName + ' ' + age)  2
 
 
-// console.log(person);
+//  let name = prompt('Ruslan')
+//  let firstName = prompt('Marushka')
+//  let age = prompt(20) 3
 
-// let s = prompt('numder1')
-// let b = prompt('numder2')
-// let c = prompt('numder3')
-// console.log()
+//   let name = 'Ruslan';
+//   let firstName = 'Marushka';
+//   let age = 20;
+//   let person = name + firstName + age
+//   console.log(person)
+// let num = +prompt(202)
+// console.log(num)  7
+// let num1 = parseInt(prompt('20990'));
+// let num2 = parseInt(prompt('-5454'));
+// let num3 = parseInt(prompt('2.656'));
+// let num4 = parseInt(prompt('7'));
+// let num1 = parseInt('20990');
+// let num2 = parseInt('-5454');
+// let num3 = parseInt('2.656');
+// let num4 = parseInt('7');
+// let result;
+// result = num4 + num3 + num2 + num1;
+// console.log(result)
+// let num_a= parseFloat(prompt('2.7382'));
+// let num_w = parseFloat(prompt('-4.9'));
+// let num_t = parseFloat(prompt('0,909'));
+// let  result = num_a  + num_t + num_w;
+// console.log(result);  9
+// let a = parseFloat(Math.round('2.2'))
+// let b = parseFloat(Math.round('2.0902'))
+// let c = parseFloat(Math.round('8.09'))
+// let w = a + b + c;
 
-// console.log(99 > 100 && 45 > 12 )
-// let a = parseFloat(3,16)
-// document.write(a)
-let name = 'ruslan';
-let farstName = 'marusjka';
-let age = 32;
-Document.write(let);
+// console.log(w)  10
+// let a = +prompt('4')
+// let b = +prompt('2')
+// let c = Math.pow(a,b)
+// console.log(c)   11
+//  let a = 100;
+//  let b = '100'; 
+//  let c = true;
+//  let d = undefined;
+//  console.log(typeof(a))
+//  console.log(typeof(b))
+//  console.log(typeof(c))
+//  console.log(typeof(d))
+// let a = 5<6;
+// let b = 5>6;
+// let c = 5>=6;
+// let v = 5===6;  16 по 19
+// let a = 10==10;20
+// let a = 10>=10;  21
+// let a = 10!=10;
+// let a = 10>10;  з 22 по 24
+// let a = 10<10;
 
 
 
 
+//  let str =  'Привіт'; 
+//  let num = 123;
+//  let flag = true;
+//  let txt = 'true';
+//  console.log(typeof(flag))  51
+// let a1 = 5+3;
+// let a2 = 5-3;
+// let a3 = 5*3;
+// let a4 = 5/3;
+// let a5 = 5%3;
+// console.log(a5)
+
+
+
+// let a6 = 5%3;
+// let a7 = 3%5;
+// let a8 = 5 + '3';
+// let a9 = '5' - 3;
+// let a10 = 75 + 'кг'
+// let heightC = parseInt("23см")
+// let width = parseInt('10см')
+// let s = heightC * width;
+
+// console.log(s)
+// let n = 3;
+// let m = 4;
+// n = n ** n; //1 катет
+// m = m ** m; // 2 катет
+// b = m + n;
+
+
+// // console.log(b)
+// let str = 'Hello world'
+// console.log(str)
+// let name = 'Ruslan'
+// let firstName = 'Marushko'
+// let age = 22;
+// let hobby = 'sport'
+// alert(name + '\n' + firstName + '\n' + age + '\n' + hobby)
+// let str1 = 'Хто'
+// let str2 = 'ти'
+// let str3 = 'такий?'
+// let concatenation = str1 + ' ' + str2+ ' ' + str3;
+// document.write(concatenation)
+    //  let str = "20";
+    //  let a = 5;
+    //  document.write(str + a + "<br/>");
+    //  document.write(str - a + "<br/>");
+    //  document.write(str * "2" + "<br/>");
+    //  document.write(str / 2 + "<br/>");
+    //  console.log( parseInt("3.14"))
+    // console.log(parseInt("-7.875"))
+    // console.log(parseInt("435"))
+    // console.log(parseInt("Вася"))
+    // let str = prompt("Enter something", "ho-ho")
+//     // console.log(str)
+// let a = +prompt('pleace number 1')
+// let b = +prompt('pleace number 2')
+// alert(a, b)
+
+// let a = prompt('Доброго вечера Иван Иванов');
+// let b = +prompt('мои поздравления что вам 32 ');
+// alert(a  + b)
+
+
+// let a = +prompt('22');
+// let b = +prompt('212');
+// let c = +prompt('2');
+// if(c<a<b){
+//     console.log(b,a,c)
+// }
+let colir = prompt('Який колір зараз горить');
+
+switch(colir){
+    case  'зелений':
+        document.write('іди');
+        break;
+        case 'синій':
+            document.write('почекай');
+            break;
+            case 'червоний':
+                document.write('стій');
+                break;
+                case 'в аварійнoму стані':
+                document.write('роби шо хочеш');
+                break;
+
+
+
+
+
+}
