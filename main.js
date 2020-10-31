bb7b5d4069dc0928977822379181950c8ae3016d
Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>
2020-10-31 10:26:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// let hello = 'hello';
+// let owu = 'owu';
+// let _com = 'com';
+// alert(hello);
+// document.write(owu);
+// console.log(_com);
+// let a = '10';
+// let b = '-999';
+// let c = '3.14'
+// let bool = true;
+// let boolean = false;
+// alert(a);
+// alert(boolean);
+// console.log(b);
+// document.write(c); // 1 && 2
+// let a = 292929;
+// let b = 202002;
+// let c = '1111';
+// let myName = 'Rus'
+// console.log(c);
+// console.log(b)
+// document.write(c);
+// // document.write(myName);  3 
+//  let name = prompt('What is your name?');
+//  let sur = prompt('What is your Sur name?');
+//  let full = prompt('What is your full name?');
+// document.write(name);
+// document.write(sur);
+// document.write(full);
+// let person = name, sur, full;
+//  let name = prompt('Ruslan');
+//  let sur = prompt('Marushka');
+//  let full = prompt('rusMaru');
+//  let name = 'Ruslan'
+//  document.write(name)
+// console.log(name)
+// let number = prompt(111)
+// // console.log(number)
+// let a = prompt('what this number?');
+// let number = '1000';
+// // console.log(number)
+// let name = prompt('What name?')
+// let surName = prompt('surName?')
+// let name = prompt('age')
+// let a = prompt('My name')
+// let b = prompt('Full name?');
+// let c = prompt('Sub name');
+// // document.write(a + name + b + fullName + c + subName)
+
+// // document.write('<h1>' + a + ' ' + b + ' '  + c + '</h1>')
+// let person = a + b + c;
+
+
+// console.log(person);
+
+// let s = prompt('numder1')
+// let b = prompt('numder2')
+// let c = prompt('numder3')
+// console.log()
+
+// console.log(99 > 100 && 45 > 12 )
+// let a = parseFloat(3,16)
+// document.write(a)
+let name = 'ruslan';
+let farstName = 'marusjka';
+let age = 32;
+Document.write(let);
+
+
+
+
