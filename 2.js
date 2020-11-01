0da1461204b8ff35d8c09c39eb1497952cc8ddf4
Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>
2020-11-01 23:03:01 +0200

    2 дз
    Signed-off-by: Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>

---
+++ 2.js
@@ -0,0 +1,143 @@
+// let  arr = ['вік', 12, 'імя', -0.39, ]
+// console.log(arr[3])
+// let arr = ['іван', 'руслан','віталік', 'володя']
+// let arr = [18, 20, 22, 25]
+
+// for(let a = 0; a < 10;a++){
+//     document.write('<div> Hellooo!!!</div>')
+// }
+// let a = 0;
+// while( a < 20){
+//     document.write('<h1> Привіт' + a +  '</h1>');
+//     a++;
+// }
+// let a = 0;
+//  while( a < 20){
+//     document.write(`<h1> Привіт ${a} </h1>`);
+//     a++;
+//  }
+// let num = [1,2,3,4,5,6,7,8,9,10];
+// console.log(num)
+// let string = ['a','s','s','d','f','g','h','j','k','r','w','o'];
+// console.log(string)
+//  let arr = ['let', 108, true, 000, 0.58, false];
+//  for (let a = 0; a <10; a++){
+//    if(`typeof arr [${a}] == Boolean `){
+//      console.log(arr[${a}])
+//    }
+//  }
+// let arr = ['let', 108, true, 000, 0.58, false];
+// // for ( i = 0; i < arr.length; i++){
+//     if (typeof arr.length[i] ==='boolean'){
+//         console.log(arr[i])  
+//     }  else {
+
+// //     }
+// for ( i = 0; i < 10; i++){
+//     document.write( `numbet : ${i}   <br>`)
+// }
+
+
+
+
+// let a = 0;
+// while(a<20){
+//     document.write(`<div> Num:  ${a} <div>`)
+//     a++
+// }
+    
+//  let arr = [122,333,444,55,222,222]
+//  for ( let a = 0; a < arr.length; a++ ){
+//      console.log( arr[a])
+//  }
+// let arr = [122,333,444,55,222,222]
+// // for (  let num  of arr){
+// //     document.write(num)
+// // }
+// for (  let num  in arr){
+//         document.write(arr[num])
+//     }
+    
+
+
+
+//  let arr = [true, 109, 'str', 309, false, -199, false, true];
+//  console.log(arr[5])
+//  console.log(arr[0]) 
+//  console.log(arr[1])
+//  console.log(arr[2])
+//  console.log(arr[3])
+
+//  for(let x = 0; x < 10; x++){
+//      console.log(x)
+//  }
+ 
+//  for(let x = 0; x < 100; x++){
+//     console.log(x)
+// }
+// for(let x = 0; x < 100; x+2){
+//     document.write(x)
+// }
+// for(let x = 0; x < 100; x++){
+//     document.write(x++)
+// }
+// for (let i = 0; i < 10; i++ ){
+//     for(let a = 0; a < 10; a++ ) {
+//         document.write(i, a + '<br>')
+//    }
+//  }
+ 
+// for ( let h = 0; h < 10; h++){
+//   for ( let m = 0; m < 20; h++ ){
+//     for ( let c = 0; c < 10; c++){
+//       document.write(h, m, c)
+//     }
+//   }
+// }
+
+//  let arr = [a, b, c ];
+//  for( let num = 0; num < arr.length; num++ ){
+//   console.log(arr[num])
+//  }
+//  for (let i = 0; i < 10; i++){
+//      for ( let a = 0; a < 5; a = a++){
+//          for ( c = 0; a < 4; a++){
+//              document.write( i, a, c );
+//          }
+//      }
+//  }
+// let arr = ['a', 'b', 'c' ];
+// let a = "";
+// for ( i = 0; i < arr.length; i++){
+//      a = a + arr[i];
+// }
+// console.log(a)
+// let arr = ['a', 'b', 'c' ];
+// let a = "";
+// let i = 0;
+// while (  i < arr.length){
+//      a = a + arr[i];
+//      i++
+// }
+// console.log(a)
+// let arr = ['a', 'b', 'c' ];
+// for ( a = 1; a <= 3; a++){
+//     arr.push(a);
+// }
+// console.log(arr)
+// let a = [1, 2, 3]
+// let i = 0;
+// for (i = 0; i > 3; i--){
+//     console.log(i)
+// }
+// let arr = ['se', 'dr', 'tr']
+// const one = arr.shift();
+// console.log( 'first', one);
+// console.log( 'arr', arr)
+// let arr = ['se', 'dr', 'tr']
+// const one = arr.pop();
+// console.log( 'finish', one);
+// console.log( 'arr', arr)
+// let num = [1, 2, 3, 4,5]
+// console.log( num.slice(0,2))
+
