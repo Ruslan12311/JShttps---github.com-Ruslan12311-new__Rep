347c7359b04a56de19806b0793ab64222d8c82a7
Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>
2020-11-03 22:57:54 +0200

    init 3 home work
    Signed-off-by: Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>

--- 3.js
+++ 3.js
@@ -156,80 +156,115 @@
 //   {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, 
 //   {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']},
 //   {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, 
-//   {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}
-// ];
-// for ( a = 0; a < users.length; a++){
-//     console.log(users[a]);
-//     for ( b = 0; b < users[0].length; b++){
-//         console.log(users[b]);
-//     }
-// }
-let users = [{
-    name: 'vasya',
-    age: 31,
-    status: false,
-    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-}, {
-    name: 'petya',
-    age: 30,
-    status: true,
-    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-}, {
-    name: 'kolya',
-    age: 29,
-    status: true,
-    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-}, {
-    name: 'olya',
-    age: 28,
-    status: false,
-    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-}, {
-    name: 'max',
-    age: 30,
-    status: true,
-    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-}, {
-    name: 'anya',
-    age: 31,
-    status: false,
-    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-}, {
-    name: 'oleg',
-    age: 28,
-    status: false,
-    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-}, {
-    name: 'andrey',
-    age: 29,
-    status: true,
-    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-}, {
-    name: 'masha',
-    age: 30,
-    status: true,
-    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-}, {
-    name: 'olya',
-    age: 31,
-    status: false,
-    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-}, {
-    name: 'max',
-    age: 31,
-    status: true,
-    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-}];
+// //   {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}
+// // ];
+// // for ( a = 0; a < users.length; a++){
+// //     console.log(users[a]);
+// //     for ( b = 0; b < users[0].length; b++){
+// //         console.log(users[b]);
+// //     }
+// // }
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+// }];
 
-// let arr = [];
-// for( user of users){
-//     arr.push(user.address)
+// // let arr = [];
+// // for( user of users){
+// //     arr.push(user.address)
+// // }
+// // console.log(arr);
+// for ( a = 0; a < users.length; a++){
+//      let user = users[a];
+//      let div = document.createElement('div');
+//      div.innerText =  `${user.name} ${user.age} ${user.status}`;
+//      document.body.appendChild(div)
+
 // }
-// console.log(arr);
-for ( a = 0; a < users.length; a++){
-     let user = users[a];
-     let div = document.createElement('div');
-     div.innerText =  `${user.name} ${user.age} ${user.status}`;
-     document.body.appendChild(div)
+// let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false},
+//  {id: 2, name: 'petya', age: 30, status: true},
+//   {id: 3, name: 'kolya', age: 29, status: true}, 
+//   {id: 4, name: 'olya', age: 28, status: false},];
+// let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'},
+//  {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'},
+//   {user_id: 4, country: 'USA', city: 'Miami'},];
+
+//   for ( const user of usersWithId){
+//       for ( const city of citiesWithId){
+//           if ( user.id === city.user_id){
+//                user.adsress = city;
+//           }
+//       }
+//   }
+// console.log(usersWithId);
+
+// console.log(eleClass);
+
+// let elementClass = document.getElementsById('content')
+/*let eleClass = elementClass.innerText;*/
+
+// let contex =    document.getElementById('content');
+// let con = contex.innerText;
+const  classRull = document.getElementsByClassName('rules rule1');
+for (let classRullElement of classRull) {
+ console.log(classRullElement)
 
 }
+
+
+
+
+
+
