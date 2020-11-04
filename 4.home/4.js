5e23963230b4af8f0ad73ac9e71fe310dd729fac
Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>
2020-11-04 15:42:05 +0200

    com 3
    Signed-off-by: Ruslan Marushka <71903355+Ruslan12311@users.noreply.github.com>

---
+++ 4.home/4.js
@@ -0,0 +1,17 @@
+
+// function funcs(num, ) {
+//     console.log(num);
+// }
+// funcs([1,2,334,34,35454,5,654,64]);
+
+// printArray(array)
+
+// function arrMath( mass, min, max ) {
+//     const array = [];
+//     for( i = 0; i < ma; i++){
+//         arrNew.push(Math.floor(Math.random() * (max - min)+ min))
+//     }
+//     return array;
+// }
+// const Arr1 = arrMath(10, 10, 100)
+// console.log(Arr1);
